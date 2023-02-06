cf2fee955064cd0d49e678fbb0f26af5a62bbbf0
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-02-06 11:35:03 +0100

    Added subscribe to features from mapService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Feature from 'ol/Feature';
 import { MapService } from './map.service';
 
 @Component({
@@ -8,7 +9,22 @@ import { MapService } from './map.service';
 })
 export class AppComponent implements OnInit {
   title = 'qGIS-OL-Test';
-  constructor(private mapService: MapService) {}
+  featureList: Feature[];
+  messageText: String;
+  
+  constructor(private mapService: MapService) {
+    this.featureList = [];
+    this.messageText = "";
+    mapService.featureEvents.features.subscribe((features) => {
+      this.featureList = features;
+      console.log(features);
+      if (features.length === 0) {
+        this.messageText = "No features found at that location";
+      } else {
+        this.messageText = `Found: ${features.length} features on location`;
+      }
+    });
+  }
   ngOnInit(): void {
     this.mapService.createMap();
   }
