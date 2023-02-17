fc0e7c4336f414b09a9081e720e1b1ef7554b0b0
Nicklas00 <nick553h@stud.kea.dk>
2023-02-17 12:55:00 +0100

    Code restructure map.service

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -46,8 +46,11 @@ export class MapService {
       { layer: 'OSM_europe', format: 'image/png' }
     ).then((wmtsLayer) => {
       const source = wmtsLayer.getSource();
-
       const vectorSource = new VectorSource({});
+      const accidentPointLayers = 'postgis:accident_points';
+      const routesLayers = 'postgis:routes';
+      const epsgProjection4326 = 'EPSG:4326';
+      const layerURL = 'http://localhost/geoserver/postgis/wms?';
 
       if (source) {
         const olMap = new Map({
@@ -69,39 +72,55 @@ export class MapService {
             zoom: 8,
           }),
         });
-        const wmsSource = new ImageWMS({
-          params: {
-            LAYERS: 'postgis:accident_points',
-            //LAYERS: 'topp:Kommuneinddeling',
-            //prettier-ignore
-            //CQL_FILTER: 'navn_tekst ilike \'%klatre%\'',
-          },
-          projection: 'EPSG:4326',
-          url: 'http://localhost/geoserver/postgis/wms?',
-        });
-        const routesSource = new ImageWMS({
-          params: {
-            LAYERS: 'postgis:routes',
-            //LAYERS: 'topp:Kommuneinddeling',
-            //prettier-ignore
-            //CQL_FILTER: 'navn_tekst ilike \'%klatre%\'',
-          },
-          projection: 'EPSG:4326',
-          url: 'http://localhost/geoserver/postgis/wms?',
-        });
+        
+        const routesSource = this.createImageWMS(routesLayers,epsgProjection4326, layerURL);
+        const wmsSource = this.createImageWMS(accidentPointLayers,epsgProjection4326, layerURL);
+        
+        this.addWMSToMap(olMap, wmsSource);
+        this.addWMSToMap(olMap, routesSource);
 
-        olMap.getView().setCenter(
-          transform(
-            //[721371, 6174352],
-            [588061, 6139595],
-            'EPSG:25832',
-            olMap.getView().getProjection()
-          )
-        );
-        olMap.addLayer(new ImageLayer({ source: wmsSource }));
-        olMap.addLayer(new ImageLayer({ source: routesSource }));
+        this.addPointer(vectorSource);
 
-        let markerCount = 0;
+        // Hook up the MouseEvents handler with our actual map:
+        this.mouseEvents.setMap(olMap);
+        
+        // Hook up the FeatureEvent to the layer we want to listen to:
+        /*this.featureEvents
+          //.setSource(wmsSource, 'topp:Kommuneinddeling')
+          .setSource(wmsSource, 'postgis:accident_points')
+          .setMouseEvents(this.mouseEvents);*/
+      }
+    });
+  }
+
+  public addWMSToMap(olMap:Map, imageWMS:ImageWMS){
+    olMap.getView().setCenter(
+      transform(
+        //[721371, 6174352],
+        [588061, 6139595],
+        'EPSG:25832',
+        olMap.getView().getProjection()
+      )
+    );
+    olMap.addLayer(new ImageLayer({ source: imageWMS }));
+  }
+
+  public createImageWMS(layers: string, projection: string, url: string): ImageWMS{
+    const wmsSource = new ImageWMS({
+      params: {
+        LAYERS: layers,
+        //LAYERS: 'topp:Kommuneinddeling',
+        //prettier-ignore
+        //CQL_FILTER: 'navn_tekst ilike \'%klatre%\'',
+      },
+      projection: projection,
+      url: url,
+    });
+    return wmsSource;
+  }
+
+  public addPointer(vectorSource: VectorSource){
+  let markerCount = 0;
 
         this.mouseEvents.clicks
           .pipe(map(mouseCoordinateConverter('CRS:84')))
@@ -128,18 +147,7 @@ export class MapService {
               markerCount = 0;
             }
           });
-
-        // Hook up the MouseEvents handler with our actual map:
-        this.mouseEvents.setMap(olMap);
-        
-        // Hook up the FeatureEvent to the layer we want to listen to:
-        /*this.featureEvents
-          //.setSource(wmsSource, 'topp:Kommuneinddeling')
-          .setSource(wmsSource, 'postgis:accident_points')
-          .setMouseEvents(this.mouseEvents);*/
-      }
-    });
-  }
+        }
 
   
 }
