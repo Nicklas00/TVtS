86b0def794a7d5cbe5c4db26281ea4d5ece01952
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-02-28 15:46:08 +0100

    added HttpClientModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { SearchComponent } from './search/search.component';
+import { RoutesSettingComponent } from './routes-setting/routes-setting.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    OverlayComponent,
+    SearchComponent,
+    RoutesSettingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
