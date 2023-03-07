4cce6a9ef049abe220f5cb84cd8f57cb0bbfab52
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-07 14:15:16 +0100

    added 5 year layer cycle

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { mouseCoordinateConverter } from 'src/openlayers-tools/mouse-events';
 import { MapService } from '../map.service';
-import { map } from 'rxjs/internal/operators/map';
 import { MarkerService } from '../marker.service';
+import { interval } from 'rxjs';
+import { ControlService } from '../control.service';
+import { Address } from '../Address';
 
 @Component({
   selector: 'app-top-bar',
@@ -10,22 +11,74 @@ import { MarkerService } from '../marker.service';
   styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent {
-  originText: string | undefined;
-  destinationText: string | undefined;
-  constructor(private mapService: MapService, private markerSerivce: MarkerService) {
-    markerSerivce.destinationCoordinates.asObservable().subscribe(coords => {
-      if(markerSerivce.destEmptyState === false){
-        this.destinationText = 'Destination: ' + coords;
+  originAddress: Address | undefined;
+  destinationAddress: Address | undefined;
+  constructor(private mapService: MapService, private controlService: ControlService, private markerService: MarkerService) {
+    controlService.destination.asObservable().subscribe(address => {
+      if(controlService.destEmptyState === false){
+        this.destinationAddress = address;
       } else {
-        this.destinationText = undefined;
+        this.destinationAddress = undefined;
       }
     })
-    markerSerivce.originCoordinates.asObservable().subscribe(coords => {
-      if(markerSerivce.originEmptyState === false) {
-        this.originText = 'Origin: ' + coords;
+    controlService.origin.asObservable().subscribe(address => {
+      if(controlService.originEmptyState === false) {
+        this.originAddress = address;
       } else {
-        this.originText = undefined;
+        this.originAddress = undefined;
       }
     })
+
+    interval(3000).subscribe(x => {
+      //this.nextLayer();
+  });
+  }
+
+  layerNumber = 0;
+  layerName = 'postgis:uag';
+  nextLayer() {
+    if (this.layerNumber > 5) {
+      this.layerNumber = 1
+    } else {
+      this.layerNumber++;
+    }
+
+    
+    switch(this.layerNumber) {
+      case 0: {
+        this.layerName = 'postgis:uag';
+        break;
+      }
+      case 1: {
+        this.layerName = 'postgis:uag_2017';
+        break;
+      }
+      case 2: {
+        this.layerName = 'postgis:uag_2018';
+        break;
+      }
+      case 3: {
+        this.layerName = 'postgis:uag_2019';
+        break;
+      }
+      case 4: {
+        this.layerName = 'postgis:uag_2020';
+        break;
+      }
+      case 5: {
+        this.layerName = 'postgis:uag_2021';
+        break;
+      }
+      case 6: {
+        this.layerName = 'postgis:uag_2022';
+        break;
+      } 
+      default: {
+        this.layerName = 'postgis:uag';
+        break;
+      }
+    }
+
+    this.mapService.pointsSource?.updateParams({'layers': this.layerName});
   }
 }
