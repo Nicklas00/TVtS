3e920a68246bd9000974067b7b308094dcb991e6
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-07 14:02:09 +0100

    added getByCoords

--- src/app/address.service.ts
+++ src/app/address.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Address } from './adress';
-import { map } from 'rxjs/internal/operators/map';
+import { Observable } from 'rxjs';
+import { Address } from './Address';
+import { Coordinate } from 'ol/coordinate';
+import { ReverseAddress } from './ReverseAddress';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AddressService {
-  private autoCompleteURL =
-    'https://dawa.aws.dk/autocomplete?fuzzy=&q=%%query%%&startfra=adresse&per_side=6';
+  private autoCompleteURL = 'https://dawa.aws.dk/autocomplete?fuzzy=&q=%%query%%&startfra=adresse&per_side=6';
+  private reverseAddressURL = 'https://api.dataforsyningen.dk/adgangsadresser/reverse?x=%%x%%&y=%%y%%&srid=4326&struktur=mini';
   constructor(private http: HttpClient) {}
 
-  //getGreeting(searchQuery: string): Observable<Address[]> {return this.http.get<Address[]>('http://localhost:8080/api/test')};
-  getGreeting(searchQuery: string): Observable<Address[]> {return this.http.get<Address[]>(this.autoCompleteURL.replace('%%query%%', searchQuery))};
+  getAddressAutocomplete(searchQuery: string): Observable<Address[]> {
+    return this.http.get<Address[]>(
+      this.autoCompleteURL.replace('%%query%%', searchQuery)
+    );
+  }
+
+  getAddressByCoordinates(coords: Coordinate): Observable<ReverseAddress> {
+    return this.http.get<ReverseAddress>(
+      this.reverseAddressURL.replace('%%x%%', '' + coords[0]).replace('%%y%%', '' + coords[1])
+    );
+  }
 }
