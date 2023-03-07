75553ee3302f2b80531ec2f65ecf6c808054d4c9
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-07 14:05:01 +0100

    added address get/select & clear search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Address } from '../Address';
 import { AddressService } from '../address.service';
+import { ControlService } from '../control.service';
 import { RoutesService } from '../routes.service';
 
 @Component({
@@ -8,32 +10,30 @@ import { RoutesService } from '../routes.service';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  searchValue = 'wildersgade';
+  searchValue = 'flakhaven 2';
+  addresses: Address[] | undefined;
 
-  constructor(private address: AddressService, private routes: RoutesService) {}
-  search() {
-    this.address
-      .getGreeting(this.searchValue)
-      .subscribe((addressData) => console.log(addressData));
+  constructor(
+    private addressService: AddressService,
+    private routes: RoutesService,
+    private controlService: ControlService
+  ) {}
+
+  select(address: Address) {
+    this.controlService.setDestination(address);
   }
 
   updateSearchField(x: any) {
     this.searchValue = x.target.value;
+    this.addressService
+    .getAddressAutocomplete(this.searchValue)
+    .subscribe((addressData) => {
+      this.addresses = addressData;
+    })
   }
 
-  test() {
-    const routeRequest = {
-      id: 0,
-      mode: 'pedestrian',
-      origin: {
-        lat: 55.393495,
-        lon: 10.376343,
-      },
-      destination: {
-        lat: 55.389638,
-        lon: 10.385509,
-      },
-    };
-    this.routes.save(routeRequest);
+  clearSearch() {
+    this.addresses = undefined;
+    this.searchValue = '';
   }
 }
