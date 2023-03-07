f140ee0417c528965ec28c4e593504e599f77781
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-07 14:11:54 +0100

    refactor

--- src/app/routeRequest.ts
+++ src/app/routeRequest.ts
@@ -1,12 +1,12 @@
 export interface routeRequest {
-    "id": number,
-    "mode": string,
-    "origin": {
-        "lat": number,
-        "lon": number
-    },
-    "destination": {
-        "lat": number,
-        "lon": number
-    }
+  id: number;
+  mode: string;
+  origin: {
+    lat: number;
+    lon: number;
+  };
+  destination: {
+    lat: number;
+    lon: number;
+  };
 }
