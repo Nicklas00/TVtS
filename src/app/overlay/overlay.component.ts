56d9248ec7ea97c67a3bb7d5e01f7c05fa00eda6
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-10 16:28:33 +0100

    Added bottomDrawer Angular render logic

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutesService } from '../routes.service';
 
 @Component({
   selector: 'app-overlay',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class OverlayComponent {
 
+  bottomDrawerDataBool: Boolean = false;
+  constructor(private routeService: RoutesService) {
+    routeService.selectedFeatures.asObservable().subscribe(features => {
+      if(features.length > 0) {
+        this.bottomDrawerDataBool = true;
+      } else {
+        this.bottomDrawerDataBool = false;
+      }
+    })
+  }
 }
