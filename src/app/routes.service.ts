944de17012f913366aae13b334cea2a0f08b00ae
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-10 16:30:41 +0100

    Added select feature and renders route on vecLayer

--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature } from 'ol';
+import { LineString, Point } from 'ol/geom';
+import { transform, transformExtent } from 'ol/proj';
+import Stroke from 'ol/style/Stroke';
+import Style from 'ol/style/Style';
+import { BehaviorSubject } from 'rxjs';
 import { ReplaySubject } from 'rxjs/internal/ReplaySubject';
+import { MapService } from './map.service';
 import { routeRequest } from './routeRequest';
 
 @Injectable({
@@ -8,7 +15,34 @@ import { routeRequest } from './routeRequest';
 })
 export class RoutesService {
   readonly summarySubject = new ReplaySubject<routeRequest>(1);
-  constructor(private httpClient: HttpClient) {}
+  readonly selectedFeatures = new BehaviorSubject<any[]>([]);
+  constructor(private httpClient: HttpClient, private mapService: MapService) {
+    
+    mapService.featureEvents.features.subscribe((features: any) => {
+      this.selectedFeatures.next(features);
+      mapService.routeVectorSource.clear();
+      if(features.length > 0) {
+        const lineString25832 = features[0].get("geometry").flatCoordinates;
+        const points = [];
+        for(let i = 0; i < lineString25832.length; i += 2) {
+          points.push(transform([lineString25832[i], lineString25832[i+1]], 'EPSG:25832', 'EPSG:3857'));
+        }
+        
+        const feature = new Feature({
+          geometry: new LineString(points)
+        });
+        feature.setStyle(
+          new Style({
+            stroke : new Stroke({
+              color : '#7fbffa',
+              width: 5
+            })
+          }
+        ));
+        mapService.routeVectorSource.addFeature(feature);
+      }
+    });
+  }
 
   save(routeRequest: routeRequest): void {
     this.httpClient
