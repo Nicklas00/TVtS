99f0a01f720b8ddbfbf8de249b803f50cf1411fe
Nicklas00 <nick553h@stud.kea.dk>
2023-03-14 14:39:17 +0100

    Combining filters works

--- src/app/layermenu-box/layermenu-box.component.ts
+++ src/app/layermenu-box/layermenu-box.component.ts
@@ -11,93 +11,89 @@ export class LayermenuBoxComponent {
   constructor(private mapService: MapService) {}
 
   filterControl(type:string, clock:string, month1:string, month2:string){
-    this.mapService.pointsSource?.updateParams({'CQL_FILTER': `time_interval_id = 1 and seriousness_id = 2`})
-    
-    /*if(type !== "0" && clock !== "0" && month1 !== null && month2 !== null){
-      //this.filterBy(type);
-      //this.searchByTime(clock);
-      this.searchByDate(month1, month2);
-      console.log(type + "  " + clock + "  " + month1 + "  " + month2);
-      
-    }else{
-      console.log("virker ikke");
+    let CQLTime = this.searchByTime(clock);
+    let CQLType = this.filterBy(type);
+    let CQLDate = this.searchByDate(month1, month2);
 
-      
-    }
-    */
-  }
+    console.log("time: " + CQLTime + "\nType: " + CQLType + "\nDate: " + CQLDate);
+    console.log(CQLType + ' and ' + CQLTime + ' and ' + CQLDate);
 
-  searchByTime(value:string){
-    if(value == "0"){
+    if(CQLTime && CQLDate && CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLType + ' and ' + CQLTime + ' and ' + CQLDate});
+      console.log("jeg er i if 1: " + CQLType + ' and ' + CQLTime + ' and ' + CQLDate);
+    }else if(CQLTime && !CQLDate && CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLType + ' and ' + CQLTime});
+      console.log("jeg er i if 2: " + CQLType + ' and ' + CQLTime);
+    }else if(!CQLTime && CQLDate && CQLType ){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLType + ' and ' + CQLDate});
+      console.log("jeg er i if 3: " + CQLType + ' and ' + CQLDate);
+    }else if(CQLTime && CQLDate && !CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLTime + ' and ' + CQLDate});
+      console.log("jeg er i if 4: " + CQLTime + ' and ' + CQLDate);     
+    }else if(!CQLTime && CQLDate && !CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLDate});
+      console.log("jeg er i if 5: " + CQLDate);
+    }else if(CQLTime && !CQLDate && !CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLTime});
+      console.log("jeg er i if 6: " + CQLTime);
+    }else if(!CQLTime && !CQLDate && CQLType){
+      this.mapService.pointsSource?.updateParams({'CQL_FILTER': CQLType});
+      console.log("jeg er i if 7: " + CQLType);
+    }else if(!CQLTime && !CQLDate && !CQLType){
       this.mapService.pointsSource?.updateParams({'CQL_FILTER': 'id > 0'});
+      console.log("jeg er i if 8: ");
+    }
+  }
+
+  searchByTime(value:string) : string | undefined{
+    if(value == "0"){
+      return undefined;
     }else{
-    this.mapService.pointsSource?.updateParams({'CQL_FILTER': `time_interval_id = '${value}'`})
-    console.log(value);
+      return `time_interval_id = '${value}'`;
     }
   }
 
-  /*switch(value){
-    case "0":{
-      this.mapService.pointsSource?.updateParams({'CQL_FILTER': `seriousness_id = '1'`})
-      break;
-    }
-    case "1":{
-      this.mapService.pointsSource?.updateParams({})
-      break;
-    }
-    case "2":{
-      this.mapService.pointsSource?.updateParams({})
-      break;
-    }
-    case "3":{
-      this.mapService.pointsSource?.updateParams({})
-      break;
-    }
-    case "4":{
-      this.mapService.pointsSource?.updateParams({})
-      break;
-    }
-  }
-  */
-
-  filterBy(value: string){
+  filterBy(value: string | undefined){
     switch(value){
       case "0":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': 'id > 0'})
+        return undefined;
         break;
       }
       case "1":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `seriousness_id = '1'`});
+        return `seriousness_id = '1'`;
         break;
       }
       case "2":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `seriousness_id = '2'`});
+        return `seriousness_id = '2'`;
         break;
       }
       case "3":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `day_type_id = '1'`});
+        return `day_type_id = '1'`;
         break;
       }
       case "4":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `day_type_id = '2'`});
+        return `day_type_id = '2'`;
         break;
       }
       case "5":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `traffic_type_id = '1'`});
+        return `traffic_type_id = '1'`;
         break;
       }
       case "6":{
-        this.mapService.pointsSource?.updateParams({'CQL_FILTER': `traffic_type_id = '2'`});
+        return `traffic_type_id = '2'`;
         break;
       }
     } 
+    return undefined;
     console.log("value: " + value);
   }
 
-  searchByDate(date1: String, date2: String){
-    console.log(date1, date2);
-    this.mapService.pointsSource?.updateParams({'CQL_FILTER': `date >= '${date1 + '-01'}' and date <= '${date2+'-01'}'`});
-    console.log(`date > '${date1}-01' and date < '${date2}-01'`);
+  searchByDate(date1: String, date2: String):string | undefined{    
+    if(!date1 || !date2){
+      return undefined;
+    }else{
+    return `date >= '${date1 + '-01'}' and date <= '${date2+'-01'}'`;
+    }
 
     
   }
