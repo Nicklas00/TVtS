15a2c9f421b6d78837e98c9cbfdc3a7aed7c4287
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-15 16:40:40 +0100

    old search removed; added MatAutocomplete

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 import { Address } from '../Address';
 import { AddressService } from '../address.service';
 import { ControlService } from '../control.service';
-import { MapService } from '../map.service';
-import { RoutesService } from '../routes.service';
-import { transform } from 'ol/proj';
 import { FormControl } from '@angular/forms';
-import { map, Observable, startWith, switchMap } from 'rxjs';
+import { Observable, startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -14,61 +11,20 @@ import { map, Observable, startWith, switchMap } from 'rxjs';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  searchValue = 'flakhaven 2';
-  addresses: Address[] | undefined;
-
   control = new FormControl('');
   options: Observable<Address[]>;
 
   constructor(
     private addressService: AddressService,
-    private routes: RoutesService,
-    private controlService: ControlService,
-    private mapService: MapService
+    private controlService: ControlService
   ) {
     this.options = this.control.valueChanges.pipe(
       startWith('Odense'),
       switchMap((value) => addressService.getAddressAutocomplete(value!))
     );
-
-    this.control.registerOnChange((address: Address) => {console.log('address.forslagstekst')});
-    
-    //this.control.
-
   }
 
-  test(x: any) {
-    console.log(this.control.getRawValue())
-    console.log(x)
-  }
-
-  select(address: Address) {
-    this.controlService.setDestination(address);
-    this.mapService
-      .testMap!.getView()
-      .setCenter(
-        transform(
-          [address.data.x, address.data.y],
-          'CRS:84',
-          this.mapService.testMap!.getView().getProjection()
-        )
-      );
-  }
-
-  updateSearchField(x: any) {
-    this.searchValue = x.target.value;
-    this.addressService.getAddressAutocomplete(this.searchValue).subscribe({
-      next: (addressData) => {
-        this.addresses = addressData;
-      },
-      error: (err) => {
-        this.addresses = undefined;
-      },
-    });
-  }
-
-  clearSearch() {
-    this.addresses = undefined;
-    this.searchValue = '';
+  selectAddress(address: Address) {
+    this.controlService.newAddress(address);
   }
 }
