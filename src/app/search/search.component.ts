84fcdf1e8e2572958a9e5ceca29b0adfc605f7e4
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-15 16:10:14 +0100

    added address list and formControl for autocomplet

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,6 +5,8 @@ import { ControlService } from '../control.service';
 import { MapService } from '../map.service';
 import { RoutesService } from '../routes.service';
 import { transform } from 'ol/proj';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -15,12 +17,30 @@ export class SearchComponent {
   searchValue = 'flakhaven 2';
   addresses: Address[] | undefined;
 
+  control = new FormControl('');
+  options: Observable<Address[]>;
+
   constructor(
     private addressService: AddressService,
     private routes: RoutesService,
     private controlService: ControlService,
     private mapService: MapService
-  ) {}
+  ) {
+    this.options = this.control.valueChanges.pipe(
+      startWith('Odense'),
+      switchMap((value) => addressService.getAddressAutocomplete(value!))
+    );
+
+    this.control.registerOnChange((address: Address) => {console.log('address.forslagstekst')});
+    
+    //this.control.
+
+  }
+
+  test(x: any) {
+    console.log(this.control.getRawValue())
+    console.log(x)
+  }
 
   select(address: Address) {
     this.controlService.setDestination(address);
