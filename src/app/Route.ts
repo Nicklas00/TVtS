4e3835c521c6d7e683a80d83eae7df38d52548ed
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-15 12:05:53 +0100

    added attr: id, mode, features

--- src/app/Route.ts
+++ src/app/Route.ts
@@ -1,7 +1,10 @@
+import { Feature } from "ol";
 import { Address } from "./Address";
 
 export interface Route {
+    id: number | undefined,
     destination: Address | undefined,
     origin: Address | undefined,
-    mode: string
+    mode: string,
+    features: Feature[]
 }
