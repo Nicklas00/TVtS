600a8becf4d90a39aa418091266bb76e7c1b31e6
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-15 16:04:46 +0100

    changed to new Route observable

--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature } from 'ol';
-import { LineString, Point } from 'ol/geom';
-import { transform, transformExtent } from 'ol/proj';
+import { LineString } from 'ol/geom';
+import { transform } from 'ol/proj';
 import Stroke from 'ol/style/Stroke';
 import Style from 'ol/style/Style';
-import { BehaviorSubject } from 'rxjs';
 import { ReplaySubject } from 'rxjs/internal/ReplaySubject';
+import { ControlService } from './control.service';
 import { MapService } from './map.service';
 import { routeRequest } from './routeRequest';
 
@@ -15,30 +15,38 @@ import { routeRequest } from './routeRequest';
 })
 export class RoutesService {
   readonly summarySubject = new ReplaySubject<routeRequest>(1);
-  readonly selectedFeatures = new BehaviorSubject<any[]>([]);
-  constructor(private httpClient: HttpClient, private mapService: MapService) {
-    
-    mapService.featureEvents.features.subscribe((features: any) => {
-      this.selectedFeatures.next(features);
+  constructor(
+    private httpClient: HttpClient,
+    private mapService: MapService,
+    private controlService: ControlService
+  ) {
+    controlService.routeObject.asObservable().subscribe((route) => {
       mapService.routeVectorSource.clear();
-      if(features.length > 0) {
-        const lineString25832 = features[0].get("geometry").flatCoordinates;
+      if (route.features.length > 0) {
+        const lineString25832 =
+          route.features[0].get('geometry').flatCoordinates;
         const points = [];
-        for(let i = 0; i < lineString25832.length; i += 2) {
-          points.push(transform([lineString25832[i], lineString25832[i+1]], 'EPSG:25832', 'EPSG:3857'));
+        for (let i = 0; i < lineString25832.length; i += 2) {
+          points.push(
+            transform(
+              [lineString25832[i], lineString25832[i + 1]],
+              'EPSG:25832',
+              'EPSG:3857'
+            )
+          );
         }
-        
+
         const feature = new Feature({
-          geometry: new LineString(points)
+          geometry: new LineString(points),
         });
         feature.setStyle(
           new Style({
-            stroke : new Stroke({
-              color : '#7fbffa',
-              width: 5
-            })
-          }
-        ));
+            stroke: new Stroke({
+              color: '#7fbffa',
+              width: 5,
+            }),
+          })
+        );
         mapService.routeVectorSource.addFeature(feature);
       }
     });
@@ -46,13 +54,9 @@ export class RoutesService {
 
   save(routeRequest: routeRequest): void {
     this.httpClient
-      .post<routeRequest>(
-        '/api/generateRoute',
-        routeRequest
-      )
+      .post<routeRequest>('/api/generateRoute', routeRequest)
       .subscribe({
         next: (responseData) => {
-          // By convention, a POST/PUT returns the stored data, so we update our state with this:
           this.summarySubject.next(responseData);
         },
         error: (e) => {
@@ -63,7 +67,7 @@ export class RoutesService {
 
   removeSelectedRoute() {
     this.summarySubject.next({
-      id: 0, 
+      id: 0,
       mode: '',
       origin: {
         lat: 0,
@@ -72,7 +76,7 @@ export class RoutesService {
       destination: {
         lat: 0,
         lon: 0,
-      }
+      },
     });
   }
 }
