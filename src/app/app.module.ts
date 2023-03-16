08d88aa520014b48f6b4841a5288c8166188d661
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2023-03-16 14:01:52 +0100

    added fontawesome support for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatCheckboxModule,
     MatFormFieldModule,
     MatSelectModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
