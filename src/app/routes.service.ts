28b48a286be9c9a3b643be1fdf1c77fe24cd183d
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-17 13:39:47 +0100

    changed to observable dependent functionality

--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -5,16 +5,24 @@ import { LineString } from 'ol/geom';
 import { transform } from 'ol/proj';
 import Stroke from 'ol/style/Stroke';
 import Style from 'ol/style/Style';
+import {
+  BehaviorSubject,
+  catchError,
+  Observable,
+  retry,
+  switchMap,
+} from 'rxjs';
 import { ReplaySubject } from 'rxjs/internal/ReplaySubject';
 import { ControlService } from './control.service';
 import { MapService } from './map.service';
+import { Route } from './Route';
 import { routeRequest } from './routeRequest';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoutesService {
-  readonly summarySubject = new ReplaySubject<routeRequest>(1);
+  routeRequest: Observable<routeRequest | undefined>;
   constructor(
     private httpClient: HttpClient,
     private mapService: MapService,
@@ -50,33 +58,75 @@ export class RoutesService {
         mapService.routeVectorSource.addFeature(feature);
       }
     });
-  }
 
-  save(routeRequest: routeRequest): void {
-    this.httpClient
-      .post<routeRequest>('/api/generateRoute', routeRequest)
-      .subscribe({
-        next: (responseData) => {
-          this.summarySubject.next(responseData);
-        },
-        error: (e) => {
-          // Relevant error handling here.
-        },
-      });
-  }
+    this.routeRequest = controlService.routeObject
+      .asObservable()
+      .pipe(switchMap(this.routeToSummaryIdConverter()));
 
-  removeSelectedRoute() {
-    this.summarySubject.next({
-      id: 0,
-      mode: '',
-      origin: {
-        lat: 0,
-        lon: 0,
-      },
-      destination: {
-        lat: 0,
-        lon: 0,
-      },
+    this.routeRequest.subscribe((routeRequest) => {
+      if (routeRequest) {
+        mapService.routesSources2?.updateParams({
+          cql_filter: 'summary_id=' + routeRequest.id,
+        });
+      } else {
+        mapService.routesSources2?.updateParams({
+          cql_filter: 'summary_id=' + 0,
+        });
+      }
     });
+  }
+
+  routeToSummaryIdConverter(): (
+    e: Route
+  ) => Observable<routeRequest | undefined> {
+    return (e) => {
+      if (e.destination && e.origin && e.setRoute) {
+        let mode = '';
+        if (e.mode === 1) {
+          mode = 'pedestrian';
+        } else {
+          mode = 'bicycle';
+        }
+
+        return this.save({
+          id: undefined,
+          mode: mode,
+          origin: {
+            lat: e.origin.data.y,
+            lon: e.origin.data.x,
+          },
+          destination: {
+            lat: e.destination.data.y,
+            lon: e.destination.data.x,
+          },
+          message: '',
+        });
+      } else {
+        return new BehaviorSubject<undefined>(undefined).asObservable();
+      }
+    };
+  }
+
+  save(routeRequest: routeRequest): Observable<routeRequest> {
+    return this.httpClient
+      .post<routeRequest>('/api/generateRoute', routeRequest)
+      .pipe(
+        retry(1),
+        catchError((err) => {
+          return new BehaviorSubject<routeRequest>({
+            id: undefined,
+            mode: 'null',
+            origin: {
+              lat: routeRequest.origin.lat,
+              lon: routeRequest.origin.lon,
+            },
+            destination: {
+              lat: routeRequest.destination.lat,
+              lon: routeRequest.destination.lon,
+            },
+            message: 'Fejl: Kunne ikke hente rute fra serveren.',
+          }).asObservable();
+        })
+      );
   }
 }
