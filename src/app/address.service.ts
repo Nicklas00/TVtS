22419f8c41e20aa3c1cd011da408bb0866049ad6
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-03-22 13:53:46 +0100

    changed mouseEvents.click to map from switchMap

--- src/app/address.service.ts
+++ src/app/address.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import {
   BehaviorSubject,
   catchError,
+  map,
   Observable,
   retry,
   switchMap,
@@ -11,14 +12,7 @@ import { Address } from './Address';
 import { Coordinate } from 'ol/coordinate';
 import { ReverseAddress } from './ReverseAddress';
 import { MapService } from './map.service';
-import {
-  get,
-  getTransform,
-  Projection,
-  ProjectionLike,
-  TransformFunction,
-} from 'ol/proj';
-import { MapBrowserEvent } from 'ol';
+import { mouseCoordinateConverter } from 'src/openlayers-tools/mouse-events';
 
 @Injectable({
   providedIn: 'root',
@@ -32,9 +26,20 @@ export class AddressService {
   address: Observable<Address> = new Observable(undefined);
 
   constructor(private http: HttpClient, private mapService: MapService) {
-    this.address = mapService.mouseEvents.clicks.pipe(
-      switchMap(this.mouseAddressConverter('CRS:84'))
-    );
+    this.address = mapService.mouseEvents.clicks
+      .pipe(map(mouseCoordinateConverter('CRS:84')))
+      .pipe(switchMap((coords) => this.getAddressByCoordinates(coords)))
+      .pipe(
+        map((reverseAddress) => {
+          return {
+            data: {
+              x: reverseAddress.x,
+              y: reverseAddress.y,
+            },
+            forslagstekst: reverseAddress.betegnelse,
+          };
+        })
+      );
   }
 
   getAddressAutocomplete(searchQuery: string): Observable<Address[]> {
@@ -60,44 +65,5 @@ export class AddressService {
           }).asObservable();
         })
       );
-  }
-
-  mouseAddressConverter(
-    targetProjectionLike: ProjectionLike
-  ): (e: MapBrowserEvent<MouseEvent>) => Observable<Address> {
-    const targetProjection = get(targetProjectionLike);
-    if (!targetProjection) {
-      throw new Error(
-        'No Proj4 definition registered for ' +
-          (targetProjectionLike instanceof Projection
-            ? targetProjectionLike.getCode()
-            : targetProjectionLike)
-      );
-    }
-    let lastKnownViewProjection: Projection | undefined;
-    let transform: TransformFunction;
-    return (e) => {
-      const viewProjection = e!.map.getView().getProjection();
-      if (viewProjection !== lastKnownViewProjection) {
-        lastKnownViewProjection = viewProjection;
-        transform =
-          viewProjection === targetProjection
-            ? (c) => c
-            : getTransform(viewProjection, targetProjection);
-      }
-      return this.getAddressByCoordinates(
-        transform(e.coordinate, undefined, undefined) as Coordinate
-      ).pipe(
-        switchMap((reverseAddress) => {
-          return new BehaviorSubject<Address>({
-            data: {
-              x: reverseAddress.x,
-              y: reverseAddress.y,
-            },
-            forslagstekst: reverseAddress.betegnelse,
-          } as Address).asObservable();
-        })
-      );
-    };
   }
 }
